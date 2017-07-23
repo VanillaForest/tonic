3b08bce2fc2ae029be059a8cf5b981e9364767c7
Lain <lain@waifu.club>
2017-07-23 15:19:34 +0000

    add cryptsetup

---
+++ mk/cryptsetup.mk
@@ -0,0 +1,19 @@
+CRYPTSETUP=1.7.4
+
+src/cryptsetup/configure:
+	mk/tarball cryptsetup https://www.kernel.org/pub/linux/utils/cryptsetup/v$(shell echo "$(CRYPTSETUP)"|cut '-d.' -f1-2)/cryptsetup-$(CRYPTSETUP).tar.xz
+
+src/cryptsetup/Makefile: src/cryptsetup/configure lib/libdevmapper.so lib/libpopt.so lib/libnettle.so include/linux/fcntl.h lib/libuuid.a
+	cd src/cryptsetup && ./configure \
+		--host=$(shell $(CC) -dumpmachine) \
+		--with-sysroot=$(CURDIR) \
+		--prefix= \
+		--sysconfdir=/etc \
+		--libdir=/lib \
+		--sbindir=/bin \
+		--disable-nls \
+		--with-crypto_backend=nettle
+
+bin/cryptsetup: src/cryptsetup/Makefile
+	make -C src/cryptsetup -j$(THREADS) V=1
+	make -C src/cryptsetup DESTDIR="$(CURDIR)" install
