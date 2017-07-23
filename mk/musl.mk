ceedebf72b0d2504852b7342591bc2eb6aa9a299
Lain <lain@waifu.club>
2017-07-23 15:18:58 +0000

    reformat musl recipe to match other files in style

--- mk/musl.mk
+++ mk/musl.mk
@@ -4,11 +4,15 @@ src/musl/Makefile:
 	mk/tarball musl https://www.musl-libc.org/releases/musl-$(MUSL_VER).tar.gz
 
 src/musl/config.mak: src/musl/Makefile
-	cd src/musl && ./configure --prefix="" --host="$(CROSS_COMPILE)" --syslibdir="/lib" --disable-wrapper
+	cd src/musl && ./configure \
+		--prefix=""
+		--host="$(CROSS_COMPILE)"
+		--syslibdir="/lib"
+		--disable-wrapper
 
 lib/libc.so: src/musl/config.mak src/musl/Makefile
 	mkdir -p lib
-	make -j$(THREADS) -C src/musl
-	make -j$(THREADS) -C src/musl DESTDIR="$(CURDIR)" install
+	make -C src/musl -j$(THREADS)
+	make -C src/musl DESTDIR="$(CURDIR)" install
 
 
