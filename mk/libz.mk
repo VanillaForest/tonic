ccda1077ae5c31c8f551c7f502054a0f45f9f3fc
Lain <lain@waifu.club>
2017-08-17 16:35:48 +0000

    add libz

---
+++ mk/libz.mk
@@ -0,0 +1,17 @@
+LIBZ_VER=1.2.8.2015.12.26
+
+src/libz/configure:
+	assets/tarball.sh libz https://sortix.org/libz/release/libz-$(LIBZ_VER).tar.gz
+
+src/libz/Makefile: src/libz/configure lib/libc.so config.mk
+	make -C src/libz distclean
+	cd src/libz && ./configure \
+		--prefix="" \
+		--host=$(shell $(CC) -dumpmachine)
+
+libz: src/libz/Makefile
+	make -C src/libz -j$(THREADS)
+	make -C src/libz V=1 DESTDIR="$(CURDIR)" install
+
+include/zlib.h \
+lib/libz.so: libz
