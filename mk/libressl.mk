1c9d81730a06bbba268ba8f61efb4d0f9f82914e
Lain <lain@waifu.club>
2017-08-17 17:22:04 +0000

    add libressl

---
+++ mk/libressl.mk
@@ -0,0 +1,16 @@
+src/libressl/configure:
+	assets/tarball.sh libressl https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.6.0.tar.gz
+
+src/libressl/Makefile: src/libressl/configure lib/libc.so include/linux/fcntl.h
+	cd src/libressl && ./configure \
+		--prefix="" \
+		--host=$(shell $(CC) -dumpmachine) \
+		--with-sysroot="$(CURDIR)"
+
+libressl: src/libressl/Makefile
+	make -C src/libressl -j$(THREADS) V=1
+	make -C src/libressl DESTDIR="$(CURDIR)" install
+
+lib/libcrypto.a \
+lib/libssl.a \
+lib/libtls.a: libressl
