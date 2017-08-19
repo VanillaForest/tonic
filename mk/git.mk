7a117aa2f68ce33ce84b54fff67b29bec8893242
Lain <lain@waifu.club>
2017-08-19 20:37:38 +0000

    fix git build

--- mk/git.mk
+++ mk/git.mk
@@ -3,7 +3,18 @@ src/git/Makefile:
 
 bin/git: src/git/Makefile lib/libz.so lib/libcurl.a
 	make -C src/git -j$(THREADS) V=1 \
+		CURL_LIBCURL="$(CURDIR)/lib/libcurl.a $(CURDIR)/lib/libssl.a $(CURDIR)/lib/libcrypto.a" \
 		CC="$(CC)" \
+		HOSTCC="$(HOSTCC)" \
+		prefix="" \
+		gitexecdir=/lib/git-core \
+		NO_TCLTK=1 \
+		NO_PYTHON=1 \
+		NO_EXPAT=1 \
+		NO_GETTEXT=1 \
 		CPPFLAGS="$(CPPFLAGS)" \
 		CFLAGS="$(CFLAGS)" \
-		LDFLAGS="$(LDFLAGS)"
+		LDFLAGS="$(LDFLAGS)" \
+		NO_REGEX=NeedsStartEnd \
+		DESTDIR="$(CURDIR)" \
+		all install
