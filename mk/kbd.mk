e4415e151ee6a26a8fbf221795d6bca2c62c5da2
Lain <lain@waifu.club>
2017-08-20 08:29:45 +0000

    kbd: add targets for building an helpful amount of keymaps

--- mk/kbd.mk
+++ mk/kbd.mk
@@ -33,6 +33,14 @@ share/kbd/%.map: src/kbd/loadkeys
 	k=$${k%.map} ; \
 	src/kbd/loadkeys -b -q $$(find src/kbd/data/keymaps/i386 -name $$k.map) > $(CURDIR)/share/kbd/$$k.map
 
-ifneq ($(KBDMAP),)
-default: share/kbd/$(KBDMAP).map
-endif
+kbd-full: share/kbd/be-latin1.map share/kbd/fr-latin1.map share/kbd/dvorak.map \
+share/kbd/dvorak-r.map share/kbd/dvorak-l.map share/kbd/br-abnt2.map \
+share/kbd/cf.map share/kbd/cz-lat2.map share/kbd/dk-latin1.map share/kbd/es.map \
+share/kbd/fi.map share/kbd/it.map share/kbd/is-latin1.map share/kbd/jp106.map \
+share/kbd/nl2.map share/kbd/no-latin1.map share/kbd/pl2.map \
+share/kbd/pt-latin1.map share/kbd/ru1.map share/kbd/se-lat6.map \
+share/kbd/tr_q-latin5.map share/kbd/trq.map share/kbd/uk.map \
+share/kbd/us-acentos.map share/kbd/us.map share/kbd/croat.map \
+share/kbd/de_CH-latin1.map share/kbd/de-latin1.map \
+share/kbd/de-latin1-nodeadkeys.map share/kbd/fr_CH-latin1.map share/kbd/hu.map \
+share/kbd/sg-latin1.map share/kbd/slovene.map
