612b77650c8d2ec8b035eda5903b59c710f56c7c
Lain <lain@waifu.club>
2017-10-14 14:18:34 +0000

    use generic name for initrd

--- mk/boot.mk
+++ mk/boot.mk
@@ -1,6 +1,6 @@
-boot/tonic.cpio.xz: init bin
+boot/initrd: init bin
 	mkdir -p boot
 	find . | \
 		grep -vf assets/cpio.exclude | \
 		cpio -R root:root -H newc -o | \
-		xz --check=crc32 --x86 --lzma2=dict=512KiB > boot/tonic.cpio.xz
+		xz --check=crc32 --x86 --lzma2=dict=512KiB > boot/initrd
